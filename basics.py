fe54d8736a664f5dbb40eabbee34a0d4568d5b3a
netr212 <netra200021kcbdr@gmail.com>
2025-03-17 22:01:31 +0000

    adding session

--- basics.py
+++ basics.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
 
 engine = create_engine("sqlite:///mydatabase.db", echo=True)
 
 conn = engine.connect()
-
 conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
-
 conn.commit()
+
+session = Session(engine)
+session.execute(text('INSERT INTO people (name, age) VALUES ("RAM", 30)'))
+session.commit()
