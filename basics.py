062b7cbc797e7f6a73b7b23c6d48165f8ee9af8a
netr212 <netra200021kcbdr@gmail.com>
2025-03-17 22:05:32 +0000

    Adding some comments

--- basics.py
+++ basics.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session
 
+# orm -> object relational mapping. 
+
 engine = create_engine("sqlite:///mydatabase.db", echo=True)
 
 conn = engine.connect()
@@ -8,5 +10,5 @@ conn.execute(text("CREATE TABLE IF NOT EXISTS people (name str, age int)"))
 conn.commit()
 
 session = Session(engine)
-session.execute(text('INSERT INTO people (name, age) VALUES ("RAM", 30)'))
+session.execute(text('INSERT INTO people (name, age) VALUES ("RAM", 30);'))
 session.commit()
