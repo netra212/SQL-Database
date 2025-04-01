39426dd46b43bac3480fdfc99b2d47bfd162dc85
netr212 <netra200021kcbdr@gmail.com>
2025-04-01 19:56:43 +0100

    DML Commands

--- SQL_CampusX/3_dml_cmds_operation.sql
+++ SQL_CampusX/3_dml_cmds_operation.sql
@@ -48,7 +48,53 @@
 -- WHERE brand_name = 'apple'
 
 -- Q2. Find all phones with price > 50000
-SELECT * FROM campusx1.smartphones
-WHERE price > 50000
+-- SELECT * FROM campusx1.smartphones
+-- WHERE price > 50000
+
+-- BETWEEN
+-- Q1. Find all phones with price ranges of 10000 and 20000
+-- SELECT * FROM campusx1.smartphones
+-- WHERE price > 10000 AND price < 20000
+
+-- SELECT * FROM campusx1.smartphones
+-- WHERE price BETWEEN 10000 AND 20000
+
+-- Q2. Find phones with rating > 80 & price < 25000
+-- SELECT * FROM campusx1.smartphones
+-- WHERE rating > 80 AND price < 25000
+
+-- Q3. Find all samsung phones with ram > 8GB
+-- SELECT * FROM campusx1.smartphones 
+-- WHERE brand_name = 'samsung' AND ram_capacity > 8
+
+-- Q4. Find all samsung phones with snapdragon processor
+-- SELECT * FROM campusx1.smartphones
+-- WHERE brand_name = 'samsung' AND processor_brand = 'snapdragon'
+
+-- QUERY Execution ORDER. 
+-- FROM JOIN WHERE GROUP BY HAVING SELECT DISTINCT ORDER BY
+-- Frank Johns Wicked Grave Haunts Several Dull Owis
+
+-- Find brands who sell phones with price > 50000
+-- SELECT DISTINCT(brand_name)
+-- FROM campusx1.smartphones
+-- WHERE price > 50000
+
+-- IN and NOT IN, If where condition ma multiple values ayo vane IN and NOT IN use garne.
+-- Fetch the brand name either the processor_brand is either snapdragon, exynos, bionic. 
+-- SELECT *
+-- FROM campusx1.smartphones
+-- WHERE processor_brand IN ('snapdragon', 'exynos', 'bionic')
+
+-- Fetch the brand name either the processor_brand NOT snapdragon, exynos, bionic. 
+-- SELECT *
+-- FROM campusx1.smartphones
+-- WHERE processor_brand NOT IN ('snapdragon', 'exynos', 'bionic')
+
+-- UPDATE 
+
+
+
+
 
 
