144f9c1aafed81c0791c0408526378b8e39d90ce
netr212 <netra200021kcbdr@gmail.com>
2025-04-01 19:35:36 +0100

    DML CMDS operation

--- SQL_CampusX/3_dml_cmds_operation.sql
+++ SQL_CampusX/3_dml_cmds_operation.sql
@@ -15,7 +15,7 @@
 -- (NULL, 'roshan', 'roshan1@gmail.com' , '12367'),
 -- (NULL, 'rahul', 'rahul1@gmail.com', '3456')
 
---- **** QUERY ****** ---
+---  QUERY ---
 -- SELECT ALL
 -- SELECT * FROM campusx1.smartphones WHERE 1 -- 1 MEANS NOT APPLYING ANY CONDITION, we need all the rows and all the columns. 
 
@@ -23,3 +23,32 @@
 -- SELECT model, price, rating FROM campusx1.smartphones 
 
 -- Alias or Renaming of cols.
+-- SELECT os AS 'Operating System', model, battery_capacity as 'maH' FROM campusx1.smartphones;
+
+
+-- Building Mathematical formula with SQL... In this case, Calculating PPI or Pixel Per inch.... Check formula on google. 
+-- SELECT model, 
+-- SQRT(resolution_width * resolution_width + resolution_height * resolution_height)/screen_size as 'PPI' 
+-- FROM campusx1.smartphones
+
+-- Creating CONSTANTS Columns. 
+-- SELECT model, 'smartphone' AS 'type' FROM campusx1.smartphones 
+
+-- DISTINCT --> to fetch unique values from columnbs,. 
+-- SELECT DISTINCT(brand_name) as 'ALL brands' FROM campusx1.smartphones 
+-- SELECT DISTINCT(processor_brand) as 'all processor' FROM campusx1.smartphones 
+
+-- UNIQUE COMBINATION.
+-- SELECT DISTINCT brand_name, processor_brand
+-- FROM campusx1.smartphones 
+
+-- Filter rows based on WHERE clause. 
+-- Q1. Find all samsung phones. 
+-- SELECT * FROM campusx1.smartphones 
+-- WHERE brand_name = 'apple'
+
+-- Q2. Find all phones with price > 50000
+SELECT * FROM campusx1.smartphones
+WHERE price > 50000
+
+
