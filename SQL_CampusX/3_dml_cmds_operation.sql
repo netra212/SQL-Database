38a67ba3656fdbb5213ae62518deb82aee268572
netr212 <netra200021kcbdr@gmail.com>
2025-04-01 19:11:39 +0100

    SQL COMMANDS

---
+++ SQL_CampusX/3_dml_cmds_operation.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE users(
+-- 	user_id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+--     name VARCHAR(255) NOT NULL, 
+--     email VARCHAR(255) NOT NULL UNIQUE, 
+--     password VARCHAR(255) NOT NULL
+-- )
+
+
+-- INSERT INTO campusx1.users (user_id, name, email, password) VALUES (NULL, "netra", "netra@gmail.com", "1234")
+-- INSERT INTO campusx1.users (user_id, name, email, password) VALUES (NULL, "ankit", "ankit@gmail.com", "123467") 
+
+-- INSERTING MULTIPLE VALUES.  --
+-- INSERT INTO campusx1.users VALUES
+-- (NULL, 'rishabh', 'rishabh@gmail.com', '12345'),
+-- (NULL, 'roshan', 'roshan1@gmail.com' , '12367'),
+-- (NULL, 'rahul', 'rahul1@gmail.com', '3456')
+
+--- **** QUERY ****** ---
+-- SELECT ALL
+-- SELECT * FROM campusx1.smartphones WHERE 1 -- 1 MEANS NOT APPLYING ANY CONDITION, we need all the rows and all the columns. 
+
+-- SELECT THREE COLUMNS
+-- SELECT model, price, rating FROM campusx1.smartphones 
+
+-- Alias or Renaming of cols.
