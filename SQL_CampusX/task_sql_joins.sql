2102045faa017cc1b1623471bc6e7a1c8079c406
netr212 <netra200021kcbdr@gmail.com>
2025-04-03 12:51:12 +0100

    SQL Joins

--- SQL_CampusX/task_sql_joins.sql
+++ SQL_CampusX/task_sql_joins.sql
@@ -47,7 +47,50 @@ GROUP BY t1.SalesPersonID
 ORDER BY unique_customers DESC LIMIT 5;
 
 -- Q-6: Sales man who has generated most revenue. Show top 5.
+SELECT t1.SalesPersonID, t3.FirstName, t3.LastName,
+ROUND(SUM(t1.Quantity * t2.Price)) AS 'total_revenue'
+FROM task_sql_joins.sales1 t1
+JOIN task_sql_joins.products t2
+ON t2.ProductID = t2.ProductID
+JOIN task_sql_joins.employees t3
+ON t1.SalesPersonID = t3.EmployeeID
+GROUP BY t1.SalesPersonID
+ORDER BY total_revenue DESC LIMIT 5;
+
 -- Q-7: List all customers who have made more than 10 purchases.
+SELECT t1.CustomerID, t2.FirstName, t2.LastName, 
+COUNT(*) 
+FROM task_sql_joins.sales1 t1
+JOIN task_sql_joins.customer t2
+ON t1.CustomerID = t2.CustomerID
+GROUP BY t1.CustomerID
+HAVING COUNT(*) > 10;
+
 -- Q-8 : List all salespeople who have made sales to more than 5 customers.
+SELECT t1.SalesPersonID, FirstName, LastName,
+COUNT(DISTINCT CustomerID) AS 'unique_customers'
+FROM task_sql_joins.sales1 t1
+JOIN task_sql_joins.employees t2
+ON t1.SalesPersonID = t2.EmployeeID
+GROUP BY t1.SalesPersonID
+HAVING unique_customers > 5;
+
 -- Q-9: List all pairs of customers who have made purchases with the same salesperson.
+SELECT CONCAT(B.FirstName,' ',B.LastName) AS 'first_customer_name', 
+CONCAT(C.FirstName,' ',C.LastName) AS 'second_customer_name'
+FROM (SELECT t1.CustomerID AS 'first_customer',
+t2.CustomerID AS 'second_customer',
+t1.SalesPersonID 
+FROM task_sql_joins.sales1 t1
+JOIN task_sql_joins.sales1 t2
+ON t1.SalesPersonID = t2.SalesPersonID
+AND t1.CustomerID != t2.CustomerID) A
+JOIN task_sql_joins.customer B
+ON A.first_customer = B.CustomerID
+JOIN task_sql_joins.customer C 
+ON A.second_customer = C.CustomerID
+
+
+
+
 
