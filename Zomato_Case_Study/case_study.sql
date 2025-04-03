6fe948c6c8c3eaa6ff2629eed21219226c2fa768
netr212 <netra200021kcbdr@gmail.com>
2025-04-03 14:05:45 +0100

    Case Study

---
+++ Zomato_Case_Study/case_study.sql
@@ -0,0 +1,39 @@
+--  1. Select a particular database. 
+-- USE zomato
+
+-- 2. Count the number of rows. 
+-- SELECT COUNT(*) FROM users;
+
+-- 3. Return Random records. 
+-- replicated sample function in pandas. 
+-- SELECT * FROM users
+-- ORDER BY rand() LIMIT 5;
+
+-- 4. Find null values Or To find the null values. 
+-- SELECT * FROM orders WHERE restaurant_rating IS NOT NULL;
+
+-- To replace null value with 0. 
+-- UPDATE orders
+-- SET restaurant_rating = 0
+-- WHERE restaurant_rating IS NULL
+
+-- 5. Find the numbers of orders placed by each customer. 
+-- SELECT t2.name, 
+-- COUNT(*) AS '#orders' 
+-- FROM orders t1
+-- JOIN users t2
+-- ON t1.user_id = t2.user_id
+-- GROUP BY t2.user_id;
+
+-- 6. Find resturants with most number of menu items. 
+-- SELECT r_name,
+-- COUNT(*) AS 'menu_items'
+-- FROM zomato.restaurants t1
+-- JOIN zomato.menu t2
+-- ON t1.r_id = t2.r_id
+-- GROUP BY t2.r_id
+
+-- 7. find number of votes and avg rating for all the restaurants. 
+
+
+
