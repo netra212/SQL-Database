2ba18b611106e2cfbe4c7fb4fc119540f49afcd5
netr212 <netra200021kcbdr@gmail.com>
2025-04-03 08:01:59 +0100

    sql joins

---
+++ SQL_CampusX/5_sql_join.sql
@@ -0,0 +1,25 @@
+-- Cross Joins. 
+SELECT * 
+FROM sql_cx_live.users1 t1
+CROSS JOIN sql_cx_live.groups t2;
+
+-- INNER JOIN.
+SELECT * 
+FROM sql_cx_live.membership t1
+INNER JOIN sql_cx_live.users1 t2
+ON t1.user_id = t2.user_id;
+
+-- LEFT JOIN.
+SELECT * 
+FROM sql_cx_live.membership t1 -- memberships table will be left table 
+LEFT JOIN sql_cx_live.users1 t2
+ON t1.user_id = t2.user_id;
+
+-- RIGHT JOIN.
+SELECT * 
+FROM sql_cx_live.membership t1  
+RIGHT JOIN sql_cx_live.users1 t2 -- users1 table will be right table
+ON t1.user_id = t2.user_id;
+
+-- FULL OUTER JOIN -- does not work on mysqlworkbench.
+
