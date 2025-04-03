d6c6a234b532798823695e9c1a341c6b495fc9a8
netr212 <netra200021kcbdr@gmail.com>
2025-04-03 14:45:49 +0100

    zomato case study

--- Zomato_Case_Study/case_study.sql
+++ Zomato_Case_Study/case_study.sql
@@ -44,16 +44,54 @@
 -- GROUP BY t1.r_id
 
 -- 8. Find the food that is being sold at most number of restaurants. 
+-- SELECT t2.f_name, 
+-- COUNT(*)
+-- FROM menu t1
+-- JOIN food t2
+-- ON t1.f_id = t2.f_id
+-- GROUP BY t1.f_id
+-- ORDER BY COUNT(*) DESC LIMIT 1;
+
 -- 9. Find restaurants with max revenue in a given month. 
+-- SELECT MONTHNAME(DATE(date)), date FROM orders
+-- SELECT r_name, SUM(amount) AS 'revenue'
+-- FROM orders t1
+-- JOIN restaurants t2
+-- ON t1.r_id = t2.r_id
+-- WHERE MONTHNAME(DATE(date)) = 'May'
+-- GROUP BY t1.r_id
+-- ORDER BY revenue DESC LIMIT 1
+
+-- month by month revenue for a particular restaurant = kfc
+SELECT MONTHNAME(DATE(date)), SUM(amount) AS 'revenue'
+FROM orders t1
+JOIN restaurants t2
+ON t1.r_id = t2.r_id
+WHERE r_name = 'kfc'
+GROUP BY MONTHNAME(DATE(date));
+
 -- 10. Find restaurants with sales > x. 
+SELECT r_name, SUM(amount) AS 'revenue'
+FROM orders t1
+JOIN restaurants t2 
+ON t1.r_id = t2.r_id
+GROUP BY t1.r_id
+HAVING revenue > 1500;
+
 -- 11. Find Customers who have never ordered. 
+SELECT user_id, name FROM users
+EXCEPT 
+SELECT t1.user_id, name FROM orders t1
+JOIN users t2
+ON t1.user_id = t2.user_id
+
 -- 12. Show order details of a particular customer in a given date range. 
+
 -- 13. Customer favorite food. 
 -- 14. Find most costly restaurants (Avg price/dish).
 -- 15. Find delivery partner compensation using the formula (#deliveries * 100 + 1000 * avg_rating)
 -- 16. Find revenue per month for a restaurant. 
--- 17. Find correlation between deliver_time and total rating. '
--- 18. 
+-- 17. Find correlation between deliver_time and total rating.
 
 
 
